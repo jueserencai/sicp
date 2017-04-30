1ccc48750a4568a5c0ef4b670757159ab0817bc6
jueserencai <wxn1905@163.com>
2017-04-30 17:04:45 +0800

    完成习题2.33

--- 2-33-accumulate.scm
+++ 2-33-accumulate.scm
@@ -10,15 +10,17 @@
 	(accumulate (lambda (x y) (cons (p x) y)) null sequence))
 
 (define (append seq1 seq2)
-	(accumulate cons seq1 seq2))
+	(accumulate cons seq2 seq1))
 
 (define (length sequence)
-	(accumulate (lambda (x y) (+ x y))  0 sequence))
+	(accumulate (lambda (x y) (+ 1 y))  0 sequence))
 
 (define square (lambda (x) (* x x)))
-
 
 (define x (list 1 2 3))
 (define y (list 4 5 6))
 
+; run test
 (map square x)
+(append x y)
+(length (append x y))
