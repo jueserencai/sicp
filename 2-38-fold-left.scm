feabeb00eddb5649010e9c49b31cbb16442b2bab
wangxingnan <wxn1905@163.com>
2017-05-03 16:19:08 +0800

    添加回答op满足的性质的问题

--- 2-38-fold-left.scm
+++ 2-38-fold-left.scm
@@ -24,3 +24,7 @@
 (fold-left / 1 x)
 (fold-right list null x)
 (fold-left list null x)
+
+; (fold-right op i (a b c)) = (op a (op b (op c i)))
+; (fold-left op i (a b c))  = (op (op (op i a) b) c)
+;; 想要op保证fold-right 和 fold-left 对任何序列产生同样的结果，op应该满足交换律。
