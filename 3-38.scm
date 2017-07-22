84486e32b8fb5939692f51dd03efe33441686965
jueserencai <wxn1905@163.com>
2017-07-22 22:07:36 +0800

    习题3.38

---
+++ 3-38.scm
@@ -0,0 +1,19 @@
+
+
+a)
+假设银行按先来先服务(first come first service) 的方式处理业务，那么 Peter 、 Paul 和 Mary 的交易有以下六种可能的排列方式：
+
+Peter -> Paul -> Mary
+Peter -> Mary -> Paul
+Paul -> Peter -> Mary
+Paul -> Mary -> Peter
+Mary -> Peter -> Paul
+Mary -> Paul -> Peter
+以上的这些排列方式会产生以下的余额值（括号里面表示的是执行操作之后的余额）：
+
+Peter (110) -> Paul (90) -> Mary (45)
+Peter (110) -> Mary (55) -> Paul (35)
+Paul (80) -> Peter (90) -> Mary (45)
+Paul (80) -> Mary (40) -> Peter (50)
+Mary (50) -> Peter (60) -> Paul (40)
+Mary (50) -> Paul (30) -> Peter (40)
